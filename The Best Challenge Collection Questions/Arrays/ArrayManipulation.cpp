b61a7df80b8320bfe164536acbaa4918f972daf0
KZyred <dung839224@gmail.com>
2024-06-20 10:15:44 +0700

    feat: Array Manipulation solve

--- The Best Challenge Collection Questions/Arrays/ArrayManipulation.cpp
+++ The Best Challenge Collection Questions/Arrays/ArrayManipulation.cpp
@@ -14,10 +14,84 @@ vector<string> split(const string &);
  *  1. INTEGER n
  *  2. 2D_INTEGER_ARRAY queries
  */
+/**
+ *
+5 3
+1 2 100
+2 5 100
+3 4 100
 
+1 2 3 4 5
+
+0 0 0 0 0
+100 100 0 0 0
+100 200 100 100 100
+100 200 200 200 100
+
+-> số lớn nhất sau các lần thực hiện là 200
+ */
+
+// tìm max 1, min 2 của 2 cặp liền nhau
+
+// cách đồ đá nhất:
+// 1. Duyệt qua mảng (trogn duyệt có duyện qua k phần tử để cộng) -> cộng O(n*k)
+// 2. Tìm max của mảng
+
+// cách 1: timeout
+// O(n*k)
+// long arrayManipulation(int n, vector<vector<int>> queries)
+// {
+//     long maxResult = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         long newItem = 0;
+//         for (int j = 0; j < queries.size(); j++)
+//         {
+//             if (i + 1 >= queries[j][0] && i + 1 <= queries[j][1])
+//             {
+//                 newItem += queries[j][2];
+//             }
+//         }
+//         maxResult = max(newItem, maxResult);
+//     }
+//     return maxResult;
+// }
+
+// cách 2: timeout
+// O(m*k)
+// long arrayManipulation(int n, vector<vector<int>> queries)
+// {
+//     vector<long> arr(n, 0);
+//     long maxResult = 0;
+//     for (int i = 0; i < queries.size(); i++)
+//     {
+//         for (int j = queries[i][0]; j <= queries[i][1]; j++)
+//         {
+//             arr[j - 1] += (long)queries[i][2];
+//             maxResult = max(arr[j - 1], maxResult);
+//         }
+//     }
+//     return maxResult;
+// }
+
+// cách 3: đúng nhất, khá thần kì
+// O(m+n)
 long arrayManipulation(int n, vector<vector<int>> queries)
 {
-    return 1;
+    vector<long> arr(n + 1, 0);
+    for (int i = 0; i < queries.size(); i++)
+    {
+        arr[queries[i][0] - 1] += (long)queries[i][2];
+        arr[queries[i][1]] -= (long)queries[i][2];
+    }
+    long maxResult = 0;
+    long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        maxResult = max(sum, maxResult);
+    }
+    return maxResult;
 }
 
 int main()
